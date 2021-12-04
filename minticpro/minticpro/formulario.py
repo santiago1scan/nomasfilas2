7676cefb3870c48442a353342fb774027ed89f61
anonymousmax1 <sntglnmndz@gmail.com>
2021-12-04 17:07:53 -0500

    otro cambio en meta

--- minticpro/minticpro/formulario.py
+++ minticpro/minticpro/formulario.py
@@ -16,5 +16,5 @@ class FormRegistrarme(UserCreationForm):
 	class Meta:
 		model = User
 
-		fields = ['username','email','password1','password2']
+		fields = ['name','age','genre','username','email','password1','password2']
 		help_texts = { k:"" for k in fields }
